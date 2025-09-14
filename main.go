6f10ac81ff0a29237e293dffab2c9fa6d9e8f99c
Heru Oktafian, ST., CTT <heru@heruoktafian.com>
2025-09-14 11:37:57 +0700

    add .env file and update main.go to use SERVER_PORT from environment

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	os "os"
+
 	utils "github.com/heru-oktafian/scafold/utils"
 )
 
 func main() {
+	// Get port from environment
+	serverPort := os.Getenv("SERVER_PORT")
+
 	app := utils.App()
-	app.Listen(":8080")
+	app.Listen(":" + serverPort)
 }
